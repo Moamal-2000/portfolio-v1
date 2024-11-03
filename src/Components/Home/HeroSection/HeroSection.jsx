7021e6cebe144a3581ede8092cb74d95b49d77d0
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-11-03 15:59:05 +0300

    feat: build structure & styles & responsiveness for HeroSection without the buttons

--- src/Components/Home/HeroSection/HeroSection.jsx
+++ src/Components/Home/HeroSection/HeroSection.jsx
@@ -1,6 +1,19 @@
 import s from "./HeroSection.module.scss";
 
 const HeroSection = () => {
-  return <section className={s.heroSection}>HeroSection</section>;
+  return (
+    <section id="home-hero" className={s.heroSection}>
+      <div className={s.content}>
+        <div>
+          <span className={s.hello}> Hello 👋</span>
+
+          <h1>
+            I'm John Anderson! A Web Developer Building Awesome Webapps And
+            Websites That Powers The Internet
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default HeroSection;
