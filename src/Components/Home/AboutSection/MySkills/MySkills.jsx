0212f8444d3952ffa3b1876ef051dd372124d18c
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-09 05:02:45 +0300

    feat: my skills now have hover color effect, live link and id

--- src/Components/Home/AboutSection/MySkills/MySkills.jsx
+++ src/Components/Home/AboutSection/MySkills/MySkills.jsx
@@ -7,8 +7,17 @@ const MySkills = () => {
       <h3 className={s.title}>My Skills</h3>
 
       <ul className={s.skills}>
-        {MY_SKILLS.map((skill) => (
-          <li key={skill}>{skill}</li>
+        {MY_SKILLS.map(({ skill, classColor, link, id }) => (
+          <li key={id}>
+            <a
+              href={link}
+              target="_blank"
+              referrerPolicy="no-referrer"
+              className={classColor}
+            >
+              {skill}
+            </a>
+          </li>
         ))}
       </ul>
     </div>
