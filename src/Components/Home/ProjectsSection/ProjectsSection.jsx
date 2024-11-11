11bf1f1556b14012aa8227fcd38690b015454941
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-11 04:05:14 +0300

    style: setup ProjectsSection's SectionTitle

--- src/Components/Home/ProjectsSection/ProjectsSection.jsx
+++ src/Components/Home/ProjectsSection/ProjectsSection.jsx
@@ -1,8 +1,14 @@
+import SectionTitle from "../../Shared/SmallComponents/SectionTitle/SectionTitle";
 import s from "./ProjectsSection.module.scss";
 
 const ProjectsSection = () => {
-  return <section className={s.projectsSection}>
-    
-  </section>;
+  return (
+    <section className={s.projectsSection}>
+      <SectionTitle
+        name="Projects 🛠️"
+        description={`Here you will find some of the personal and clients projects that I created with the project details`}
+      />
+    </section>
+  );
 };
 export default ProjectsSection;
