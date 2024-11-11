989fd577061b2fec33953cf56d009306d356460c
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-11 03:58:52 +0300

    init: ProjectsSection component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import ThemeChanger from "../Shared/SmallComponents/ThemeChange/ThemeChanger";
 import AboutSection from "./AboutSection/AboutSection";
 import HeroSection from "./HeroSection/HeroSection";
 import s from "./Home.module.scss";
+import ProjectsSection from "./ProjectsSection/ProjectsSection";
 import SkillsShowcase from "./SkillsShowcase/SkillsShowcase";
 
 const Home = () => {
@@ -13,6 +14,11 @@ const Home = () => {
 
       <AboutSection />
       <SkillsShowcase />
+
+      <div className="container">
+        <ProjectsSection />
+      </div>
+
       <ThemeChanger />
     </main>
   );
---
+++ src/Components/Home/ProjectsSection/ProjectsSection.jsx
@@ -0,0 +1,8 @@
+import s from "./ProjectsSection.module.scss";
+
+const ProjectsSection = () => {
+  return <section className={s.projectsSection}>
+    
+  </section>;
+};
+export default ProjectsSection;
