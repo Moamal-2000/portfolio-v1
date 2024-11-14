319e1aeee9e9bb5e33325c609a8149d35ef99a87
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-15 02:46:36 +0300

    style: Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
+import { NAV_LINKS } from "src/Data/variables";
 import s from "./Footer.module.scss";
 
 const Footer = () => {
-  return <footer className={s.footer}>
-    
-  </footer>;
+  return (
+    <footer className={s.footer}>
+      <nav className={s.nav}>
+        <ul>
+          {NAV_LINKS.map(({ name, link }) => (
+            <li key={`${name}`}>
+              <a href={link}>{name}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </footer>
+  );
 };
 export default Footer;
