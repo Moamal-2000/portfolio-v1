90d316663d816ba28a2288bdb9ddd647febc883a
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-15 02:21:58 +0300

    style: ContactForm

--- src/Components/Home/ContactSection/ContactForm/ContactForm.jsx
+++ src/Components/Home/ContactSection/ContactForm/ContactForm.jsx
@@ -1,3 +1,4 @@
+import { contactFormBg } from "src/Assets/images";
 import s from "./ContactForm.module.scss";
 
 const ContactForm = () => {
@@ -7,7 +8,35 @@ const ContactForm = () => {
 
   return (
     <form className={s.contactForm} onSubmit={handleSubmit}>
-      ContactForm
+      <div className={s.form}>
+        <div className={s.input}>
+          <label htmlFor="name">Name</label>
+          <input placeholder="Enter Your Name" type="text" />
+        </div>
+
+        <div className={s.input}>
+          <label htmlFor="name">Email</label>
+          <input placeholder="Enter Your Email" type="text" />
+        </div>
+
+        <div className={s.input}>
+          <label htmlFor="name">Message</label>
+          <textarea
+            placeholder="Enter Your Message"
+            name="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+
+        <button type="submit" className={s.submitButton}>
+          Submit
+        </button>
+      </div>
+
+      <div className={s.imgHolder}>
+        <img src={contactFormBg} alt="" />
+      </div>
     </form>
   );
 };
