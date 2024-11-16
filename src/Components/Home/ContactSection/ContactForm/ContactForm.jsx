b51303bb43ac04e9cdf3dfedb2f92e8fee5f653b
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-16 09:49:05 +0300

    feat: some changes in ContactForm, controlled inputs, error handling, empty inputs on success

--- src/Components/Home/ContactSection/ContactForm/ContactForm.jsx
+++ src/Components/Home/ContactSection/ContactForm/ContactForm.jsx
@@ -1,20 +1,31 @@
 import emailjs from "@emailjs/browser";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { contactFormBg } from "src/Assets/images";
 import s from "./ContactForm.module.scss";
 
 const ContactForm = () => {
   const formRef = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
     sendMessage();
   }
 
-  function sendMessage() {
-    emailjs.sendForm("service_ro1lghg", "template_m25p1w3", formRef.current, {
-      publicKey: "SyWSSmy9K9xVdF89p",
-    });
+  async function sendMessage() {
+    try {
+      emailjs.sendForm("service_ro1lghg", "template_m25p1w3", formRef.current, {
+        publicKey: "SyWSSmy9K9xVdF89p",
+      });
+
+      setName("");
+      setEmail("");
+      setMessage("");
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -22,12 +33,22 @@ const ContactForm = () => {
       <div className={s.form}>
         <div className={s.input}>
           <label htmlFor="name">Name</label>
-          <input placeholder="Enter Your Name" type="text" />
+          <input
+            placeholder="Enter Your Name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div className={s.input}>
           <label htmlFor="name">Email</label>
-          <input placeholder="Enter Your Email" type="text" />
+          <input
+            placeholder="Enter Your Email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div className={s.input}>
@@ -37,6 +58,8 @@ const ContactForm = () => {
             name="message"
             cols="30"
             rows="10"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
         </div>
 
