5a17282d8010506b25e360baebd4e43c0c3d958a
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-16 07:36:59 +0300

    feat: create copyrights text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,6 +15,25 @@ const Footer = () => {
         </ul>
 
         <SocialMedia />
+
+        <p className={s.copyrights}>
+          Website designed by{" "}
+          <a
+            href="https://www.linkedin.com/in/rammcodes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ram Maheshwari
+          </a>
+          &nbsp; And Developed by{" "}
+          <a
+            href="https://www.linkedin.com/in/moamal-alaa"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Moamal Alaa
+          </a>
+        </p>
       </nav>
     </footer>
   );
