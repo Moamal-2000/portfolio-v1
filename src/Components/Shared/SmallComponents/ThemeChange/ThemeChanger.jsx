205b0440ed3886ba6fbb58a653d17e465b096c46
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-17 10:30:33 +0300

    chore: use SvgIcon component for close button in ThemeChanger

--- src/Components/Shared/SmallComponents/ThemeChange/ThemeChanger.jsx
+++ src/Components/Shared/SmallComponents/ThemeChange/ThemeChanger.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { colorPicker, gearIcon, textIcon, themeIcon } from "src/Assets/images";
 import { hexToRgb } from "src/Functions/helper";
 import useLocalStorage from "src/Hooks/Helper/useLocalStorage";
+import SvgIcon from "../SvgIcon";
 import s from "./ThemeChanger.module.scss";
 
 const ThemeChanger = () => {
@@ -82,9 +83,7 @@ const ThemeChanger = () => {
           onClick={() => setIsMenuActive(false)}
           title="Close"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-          </svg>
+          <SvgIcon name="xMark" />
         </button>
       </div>
     </>
