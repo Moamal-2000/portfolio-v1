da88ee5c20ea50115627a82c03bbf235289e7757
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-18 03:22:24 +0300

    init: GlobalContext and wrap it on the App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import GlobalContextProvider from "./Context/GlobalContext.jsx";
 import "./Styles/index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <GlobalContextProvider>
+      <App />
+    </GlobalContextProvider>
   </StrictMode>
 );
