b6762afe4e3f988570c8a7d2f19ab0dcfc27a9f3
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-19 05:42:22 +0300

    feat: style active section on scroll for header nav links

--- src/Components/Header/HeaderNav/HeaderNav.jsx
+++ src/Components/Header/HeaderNav/HeaderNav.jsx
@@ -1,21 +1,49 @@
+import { useEffect, useRef } from "react";
 import { useGlobalContext } from "src/Context/GlobalContext";
 import { NAV_LINKS } from "src/Data/staticData";
 import s from "./HeaderNav.module.scss";
 
 const HeaderNav = () => {
   const { activeSection } = useGlobalContext();
+  const ulRef = useRef();
+  const activeLineRef = useRef();
+  const debounceRef = useRef();
+
+  function getActiveLinePosition() {
+    const navLinks = [...ulRef?.current?.children];
+    const activeLineEle = activeLineRef?.current;
+    let linePosition = 0;
+
+    for (let index = 0; index < navLinks.length; index++) {
+      const link = navLinks[index];
+      const isActive = link.dataset.active === "true";
+      const linkWidth = link.offsetWidth;
+
+      if (isActive) {
+        activeLineEle.style.width = linkWidth + "px";
+        break;
+      }
+
+      linePosition += linkWidth;
+    }
+    activeLineEle.style.left = linePosition + "px";
+  }
+
+  useEffect(() => {
+    clearTimeout(debounceRef.current);
+    debounceRef.current = setTimeout(getActiveLinePosition, 400);
+  }, [activeSection]);
 
   return (
     <nav className={s.nav}>
-      <ul>
+      <ul ref={ulRef}>
         {NAV_LINKS.map(({ name, link }) => (
-          <li
-            key={`${name}`}
-            className={activeSection === name ? s.active : ""}
-          >
+          <li key={`${name}`} data-active={activeSection === name}>
             <a href={link}>{name}</a>
           </li>
         ))}
+
+        <li ref={activeLineRef} className={s.activeLine} />
       </ul>
     </nav>
   );
